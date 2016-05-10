54d1b4863978fb21d008810b6bc2e9870dc351d4
Pxxl13 <luca.schneider@gmx.ch>
2016-05-10 09:35:54 +0200

    worked on calculator, added button events and a little bit of output

--- calculator/scripts/calculator.js
+++ calculator/scripts/calculator.js
@@ -9,7 +9,26 @@
  * UI
  */
 window.addEventListener('load', function() {
+    document.addEventListener('click', function(evnt) {
+
+        if(evnt.target.attributes.getNamedItem('class') != null){
+            if (evnt.target.attributes.getNamedItem('class').nodeValue == 'number') {
+                console.log('pressed number');
+                document.getElementById('input').innerHTML += evnt.target.attributes.getNamedItem('value').nodeValue;
+
+            }
+            if (evnt.target.attributes.getNamedItem('class').nodeValue == 'operator') {
+                console.log('pressed operator');
+                document.getElementById('output').innerHTML = document.getElementById('input').innerHTML;
+                document.getElementById('input').innerHTML = '';
+            }
+            if (evnt.target.attributes.getNamedItem('class').nodeValue == 'command') {
+                console.log('pressed command');
+            }
+        }
 
 
+
+    });
 
 });
