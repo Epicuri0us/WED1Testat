9c133c6cb9039e54df55607f0854377fe2f3a5d5
Pxxl13 <luca.schneider@gmx.ch>
2016-05-12 17:32:47 +0200

    validated javascript, javascript finished

--- calculator/scripts/calculator.js
+++ calculator/scripts/calculator.js
@@ -1,103 +1,117 @@
 /**
  * core
  */
-
-//handles the symbols for calculations
-function calculate(a,b,operation){
-    switch(operation) {
-        case "+": return sum(a,b);
-            break;
-        case "−": return difference(a,b);
-            break;
-        case "∗": return product(a,b);
-            break;
-        case "÷": return quotient(a,b);
-            break;
-        default:
-            break;
-    }
-}
-
 //sums to numbers with NaN-detection
-function sum(a,b){
-    if(isNaN(a+b)){
+function sum(a, b) {
+    'use strict';
+    if (isNaN(a + b)) {
         return 'Invalid Calculation';
     }
-    return a+b;
+    return a + b;
 }
 
 //returns the difference between two numbers with NaN-detection
-function difference(a,b){
-    if(isNaN(a-b)){
+function difference(a, b) {
+    'use strict';
+    if (isNaN(a - b)) {
         return 'Invalid Calculation';
     }
-    return a-b;
+    return a - b;
 }
 
 //returns the product of two numbers with NaN-detection
-function product(a,b){
-    if(isNaN(a*b)){
+function product(a, b) {
+    'use strict';
+    if (isNaN(a * b)) {
         return 'Invalid Calculation';
     }
-    return a*b;
+    return a * b;
 }
 
 //returns the quotient of two numbers with NaN- and Infinite-detection
-function quotient(a,b){
-    if(!isFinite(a/b) | (isNaN(a/b))){
+function quotient(a, b) {
+    'use strict';
+    if (!isFinite(a / b) || (isNaN(a / b))) {
         return 'Invalid Calculation';
     }
-    return a/b;
+    return a / b;
+}
+
+//handles the symbols for calculations
+function calculate(a, b, operation) {
+    'use strict';
+    switch (operation) {
+        case "+":
+            return sum(a, b);
+        case "−":
+            return difference(a, b);
+        case "∗":
+            return product(a, b);
+        case "÷":
+            return quotient(a, b);
+        default:
+            return 'Invalid Calculation';
+    }
 }
 
 /**
  * UI
  */
-window.addEventListener('load', function() {
+//check the Welcome-HTML
+function checkWelcome() {
+    'use strict';
+    if (document.getElementById('output').innerHTML === "Welcome") {
+        document.getElementById('output').innerHTML = "";
+    }
+}
+
+window.addEventListener('load', function () {
+    'use strict';
     document.getElementById('output').innerHTML = "Welcome";
     //for calculations
     var input = '';
     var output = '';
     var operator = '';
-    document.addEventListener('click', function(evnt) {
+    document.addEventListener('click', function (evnt) {
         //button pressed
-        if(evnt.target.attributes.getNamedItem('class') != null){
+        if (evnt.target.attributes.getNamedItem('class') !== null) {
             checkWelcome();
 
             //number pressed
-            if (evnt.target.attributes.getNamedItem('class').nodeValue == 'number') {
+            if (evnt.target.attributes.getNamedItem('class').nodeValue === 'number') {
                 console.log('pressed number');
                 input += evnt.target.attributes.getNamedItem('value').nodeValue;
             }
 
             //operator pressed
-            if (evnt.target.attributes.getNamedItem('class').nodeValue == 'operator') {
+            if (evnt.target.attributes.getNamedItem('class').nodeValue === 'operator') {
                 console.log('pressed operator');
-                var id = evnt.target.attributes.getNamedItem('id').nodeValue;
-                operator = document.getElementById(id).innerHTML;
-                if(output == ''){
+                var operatorid = evnt.target.attributes.getNamedItem('id').nodeValue;
+                operator = document.getElementById(operatorid).innerHTML;
+                if (output === '') {
                     output = input;
                     input = '';
                 }
             }
 
             //command pressed
-            if (evnt.target.attributes.getNamedItem('class').nodeValue == 'command') {
+            if (evnt.target.attributes.getNamedItem('class').nodeValue === 'command') {
                 console.log('pressed command');
-                var id = evnt.target.attributes.getNamedItem('id').nodeValue;
-                var command = document.getElementById(id).innerHTML;
+                var commandid = evnt.target.attributes.getNamedItem('id').nodeValue;
+                var command = document.getElementById(commandid).innerHTML;
 
                 //C-button pressed
-                if(command == "C"){
+                if (command === "C") {
                     input = '';
                     output = '';
                     operator = '';
-                }else {
+                } else {
                     //equal-button pressed
-                    var a, b;
+                    var a;
+                    var b;
                     a = parseFloat(output);
                     b = parseFloat(input);
-                    input = calculate(a,b,operator);
+                    input = calculate(a, b, operator);
                     output = '';
                     operator = '';
                 }
@@ -105,14 +119,7 @@ window.addEventListener('load', function() {
 
             //display the output
             document.getElementById('input').innerHTML = input;
-            document.getElementById('output').innerHTML = output+ " " + operator;
+            document.getElementById('output').innerHTML = output + " " + operator;
         }
     });
 });
-
-//check the Welcome-HTML
-function checkWelcome(){
-    if(document.getElementById('output').innerHTML == "Welcome"){
-        document.getElementById('output').innerHTML = "";
-    }
-}
